99a49dc1d46ce2c98bca7828f890229a834e645b
TonmoyKabir <99064631+TonmoyKabir@users.noreply.github.com>
2022-04-30 01:55:30 +0600

    Add files via upload

---
+++ RiceAgencyUnitTest/AdminControllerTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rice_Agency.Controllers;
+using Rice_Agency.DAL;
+using Rice_Agency.Models.Home;
+using Rice_Agency.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+
+namespace RiceAgencyUnitTest
+{
+
+    [TestClass]
+    public class AdminControllerTest
+    {
+        [TestMethod]
+        public void Dashboard()
+        {
+            //Arrange
+            AdminController Controller = new AdminController();
+
+            //Act
+            ViewResult result = Controller.Dashboard() as ViewResult;
+
+            //Assert
+             Assert.IsNotNull(result);
+
+        }
+        [TestMethod]
+        public void ProductAdd()
+        {
+            //Arrange
+            AdminController Controller = new AdminController();
+
+            //Act
+            ViewResult result = Controller.ProductAdd(2) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Product()
+        {
+            //Arrange
+            AdminController Controller = new AdminController();
+
+            //Act
+            ViewResult result = Controller.Product() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
+
